c93ef30ab28f74a4425a87065f854b85f72db3e6
marcin-chwalek <m.chw@outlook.com>
2017-08-19 16:44:28 +0200

    Initial projects structure

--- JsonSad.Tests/Properties/AssemblyInfo.cs
+++ JsonSad.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonSad")]
+[assembly: AssemblyTitle("JsonSad.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("JsonSad")]
+[assembly: AssemblyProduct("JsonSad.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c3b7ece3-0b28-43ac-bca9-7a0cf1cbd71a")]
+[assembly: Guid("d594e027-8b1f-4290-aa6f-d9e5c45f845f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("JsonSad.Tests")]

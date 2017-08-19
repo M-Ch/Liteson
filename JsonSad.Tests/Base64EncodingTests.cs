09f58e47e912e94fc5d216b8bea47f569cd10de4
marcin-chwalek <m.chw@outlook.com>
2017-08-19 18:33:19 +0200

    Base64 encoder writing directly to stream

---
+++ JsonSad.Tests/Base64EncodingTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace JsonSad.Tests
+{
+	public class Base64EncodingTests
+	{
+		[Fact]
+		public void SimpleBlock()
+		{
+			var data = new byte[] {1, 2, 3};
+			Base64.Write(data).ShouldBeEquivalentTo(Convert.ToBase64String(data));
+		}
+
+		[Fact]
+		public void Zeros()
+		{
+			var data = new byte[] {0,0,0};
+			Base64.Write(data).ShouldBeEquivalentTo(Convert.ToBase64String(data));
+		}
+
+		[Fact]
+		public void EmptyArray() => Base64.Write(Array.Empty<byte>()).ShouldBeEquivalentTo(Convert.ToBase64String(Array.Empty<byte>()));
+
+		[Fact]
+		public void Null() => Assert.Throws<ArgumentNullException>(() => Base64.Write(null));
+
+		[Fact]
+		public void IncompleteBlockOneByte()
+		{
+			var data = new byte[] {123, 98, 200, 45};
+			Base64.Write(data).ShouldBeEquivalentTo(Convert.ToBase64String(data));
+		}
+
+		[Fact]
+		public void IncompleteBlockTwoBytes()
+		{
+			var data = new byte[] {15, 243, 122, 99, 87};
+			Base64.Write(data).ShouldBeEquivalentTo(Convert.ToBase64String(data));
+		}
+
+		[Fact]
+		public void LoremIpsum()
+		{
+			var data = Encoding.UTF8.GetBytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id purus ultrices diam elementum venenatis convallis vitae erat. Fusce facilisis quam in fermentum posuere. Cras in ornare eros, ut sollicitudin sem. Aenean nec faucibus augue. Curabitur viverra ante nec ipsum mollis, non ornare urna fringilla. Cras id mollis ipsum, eget ullamcorper risus. Nunc at magna magna. Quisque tempor suscipit interdum. Vivamus tincidunt, mi eu luctus cursus, diam ipsum blandit augue, ut tincidunt urna dui eu magna. Curabitur nunc lorem, viverra nec nisl in, facilisis auctor nibh. Curabitur feugiat volutpat tempor. Nam facilisis consequat aliquet. ");
+
+			Base64.Write(data).ShouldBeEquivalentTo(Convert.ToBase64String(data));
+		}
+	}
+}
--- JsonSad/Base64.cs
+++ JsonSad/Base64.cs
@@ -16,10 +16,42 @@ namespace JsonSad
 
 		public static void Write(StringWriter target, byte[] data)
 		{
-			var blockCount = data.Length / 3;
+			if (data == null)
+				throw new ArgumentNullException(nameof(data));
+
+			const int blockSize = 3;
+			var blockCount = data.Length / blockSize;
 			for (var a = 0; a<blockCount;a++)
 			{
+				var index = a * blockSize;
+				target.Write(Lookup[data[index] >> 2]);
+				target.Write(Lookup[(data[index] & 0b0000_0011) << 4 | (data[index+1] >> 4)]);
+				target.Write(Lookup[(data[index + 1] & 0b0000_1111) << 2 | data[index + 2] >> 6]);
+				target.Write(Lookup[data[index + 2] & 0b0011_1111]);
 			}
+
+			var remaining = data.Length % blockSize;
+			if (remaining == 0)
+				return;
+
+			var lastBlock = new byte[blockSize];
+			Array.Copy(data, blockCount * blockSize, lastBlock, 0, remaining);
+			target.Write(Lookup[lastBlock[0] >> 2]);
+			if (remaining >= 1)
+			{
+				target.Write(Lookup[(lastBlock[0] & 0b0000_0011) << 4 | (lastBlock[1] >> 4)]);
+				if(remaining == 2)
+					target.Write(Lookup[(lastBlock[1] & 0b0000_1111) << 2 | lastBlock[2] >> 6]);
+			}
+
+			target.Write(remaining == 1 ? "==" : "=");
+		}
+
+		public static string Write(byte[] data)
+		{
+			var sw = new StringWriter();
+			Write(sw, data);
+			return sw.ToString();
 		}
 	}
 }
